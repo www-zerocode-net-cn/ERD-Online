a1a8c5955f26a69d0a25b7bd789bbbf3931a5630
whaty <liangcan.jxjy.edu@gmail.com>
2020-11-20 17:27:40 +0800

    ERD-ONLINE v2.0.3 提供Web、Windows、MAC、Linux多端打包

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow() {
     mainWindow.loadFile('./build/index.html')
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
