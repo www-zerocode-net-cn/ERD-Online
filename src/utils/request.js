4432cc0a6b80879bd047671377fef7578b9c2320
whaty <liangcan.jxjy.edu@gmail.com>
2021-02-25 18:44:48 +0800

    ERD-ONLINE v3.0.1 更新图标

--- src/utils/request.js
+++ src/utils/request.js
@@ -84,10 +84,10 @@ const errorHandler = error => {
  * 配置request请求时的默认参数
  */
 const request = extend({
-    //本地
-    prefix: 'http://erd-online/erd',
-    // // //生产
-    // prefix: 'https://erd.java2e.com/erd',
+    // //本地
+    // prefix: 'http://erd-online/erd',
+    // //生产
+    prefix: 'https://erd.java2e.com/erd',
     errorHandler, // 默认错误处理
 });
 
