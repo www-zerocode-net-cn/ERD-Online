ba423fab4736e49cc45d40628ad72c15016321d4
liangcan.jxjy.edu@gmail.com <liangcan.jxjy.edu@gmail.com>
2021-08-19 18:14:19 +0800

    erd online 改版

---
+++ src/pages/project/home/index.tsx
@@ -0,0 +1,255 @@
+import React from "react";
+import ZeroCodeGridLayout from "@/components/ZeroCodeGridLayout";
+import {Image} from "antd";
+import style from './index.less';
+import {
+  A6CBC655ACD1,
+  A9C620A72E3,
+  AAA800E5216E,
+  AC0DC9DF456A,
+  AE339CAE1EB4,
+  B0062A6BDF52,
+  B291DCDC658,
+  B49B56FB5F,
+  B4A8891148F,
+  B56EC4F2D9B,
+  B57EBF5,
+  B62FDB3228AE,
+  B8E69BF647,
+  B9B052826F,
+  C0083E21A8,
+  C184E904BF5,
+  C61117D62,
+  CBF593C20DB,
+  CE425719BB,
+  CFAE62FB6D44,
+  CFBE1A9CB30,
+  D04DC,
+  D05FA0EA492,
+  D1F781207,
+  D5EA691B8E3,
+  DA1CB00434B,
+  E079FA73D7,
+  E0DF67094,
+  E5C34A833DE,
+  EC5E7DBCDF6F,
+  F1DE6A851EC,
+  F3A6F893D,
+  F706F34,
+  F7254AD1,
+  F8886065B3D,
+  FAA2,
+  FB25F66C98CC,
+  FB695426DAA1,
+} from './gridLayouts';
+
+export default class MyFirstGrid extends React.Component {
+  render() {
+    return (
+      <>
+        <div key="320400F6-E186-4B63-9E06-08446B57EBF5" className={style.B57EBF5}><ZeroCodeGridLayout layout={B57EBF5}>
+          <div key="03FC057B-1296-48C0-B0E0-0F1DE6A851EC" className={style.F1DE6A851EC}><ZeroCodeGridLayout
+            layout={F1DE6A851EC}>
+            <div key="3C23EFE7-ABDF-4769-96E9-430E0DF67094" className={style.E0DF67094}><ZeroCodeGridLayout
+              layout={E0DF67094}>
+              <div key="D355114D-A813-4154-A227-B0062A6BDF52" className={style.B0062A6BDF52}><ZeroCodeGridLayout
+                layout={B0062A6BDF52}>
+                <div key="03394707-1ECF-4D1E-A838-26E079FA73D7" className={style.E079FA73D7}><ZeroCodeGridLayout
+                  layout={E079FA73D7}>
+                  <div key="D9936EA7-9DA1-4A5C-AFE9-5E0EF3E180AD">
+                    <div className={style.E0EF3E180AD}>Metadata modeling</div>
+                  </div>
+                  <div key="339150DA-BDF7-4A54-9205-7DAE6C568C78"><Image preview={false} src={"/erd/元数据建模.svg"}/></div>
+                </ZeroCodeGridLayout></div>
+              </ZeroCodeGridLayout></div>
+              <div key="A9A1628C-718E-4807-B1A4-B245C4FFFF40" className={style.B245C4FFFF40}></div>
+              <div key="38A5F68C-C6FA-420B-942E-1F8886065B3D" className={style.F8886065B3D}><ZeroCodeGridLayout
+                layout={F8886065B3D}>
+                <div key="4E4A78B6-1AD8-49EE-A379-1ED72AC85E45" className={style.ED72AC85E45}></div>
+              </ZeroCodeGridLayout></div>
+              <div key="72F8F22A-B889-4453-BD3D-8D05FA0EA492" className={style.D05FA0EA492}><ZeroCodeGridLayout
+                layout={D05FA0EA492}>
+                <div key="0880D20C-BAE0-4095-9D76-0197C626995B">
+                  <div className={style.C626995B}>Home</div>
+                </div>
+                <div key="E7D89249-2754-4911-831D-A9CA5FE66713"><Image preview={false} src={"/erd/首页.svg"}/></div>
+              </ZeroCodeGridLayout></div>
+              <div key="41BB0E66-AF8E-4F90-816D-6B56EC4F2D9B" className={style.B56EC4F2D9B}><ZeroCodeGridLayout
+                layout={B56EC4F2D9B}>
+                <div key="81D9C25D-7F16-4E10-A9A1-9FEEC3D8B965">
+                  <div className={style.FEEC3D8B965}>System management</div>
+                </div>
+                <div key="8CC1F54A-E5C4-4046-B2CF-D668EA63D7F9"><Image preview={false} src={"/erd/系统管理.svg"}/></div>
+              </ZeroCodeGridLayout></div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="CC6A1E8E-E6FB-449B-815B-AC0DC9DF456A" className={style.AC0DC9DF456A}><ZeroCodeGridLayout
+            layout={AC0DC9DF456A}>
+            <div key="18864029-58E6-411B-B1D1-AAA800E5216E" className={style.AAA800E5216E}><ZeroCodeGridLayout
+              layout={AAA800E5216E}>
+              <div key="B6BBAB01-55BC-4A33-BA3E-6E3BD2585902">
+                <div className={style.E3BD2585902}>全部项目</div>
+              </div>
+              <div key="1F027A89-C6B8-42E7-8C33-512026EBFD78"><Image preview={false} src={"/erd/Icon/Search.svg"}/>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="769376CA-E632-4E73-995E-FB25F66C98CC" className={style.FB25F66C98CC}><ZeroCodeGridLayout
+            layout={FB25F66C98CC}>
+            <div key="FBABFF56-24D1-476D-B2E7-62C0083E21A8" className={style.C0083E21A8}><ZeroCodeGridLayout
+              layout={C0083E21A8}>
+              <div key="2E14416A-CE82-4166-94D4-F1E985B674CA">
+                <div className={style.F1E985B674CA}>请输入项目名称</div>
+              </div>
+              <div key="6C0E0478-503D-41BD-9728-60205DB40747"><Image preview={false} src={"/erd/Icon/Search.svg"}/>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="156FD0B8-1BE3-4DFD-9953-03CE425719BB" className={style.CE425719BB}><ZeroCodeGridLayout
+            layout={CE425719BB}>
+            <div key="E330F30B-03CA-4884-83E5-6CBF593C20DB" className={style.CBF593C20DB}><ZeroCodeGridLayout
+              layout={CBF593C20DB}>
+              <div key="5EB1FD2D-5610-466F-B993-F50AD97D98AD" className={style.F50AD97D98AD}></div>
+              <div key="EC5CA6D0-3B2C-444D-B50B-456559FD9F0F">
+                <div className={style.FD9F0F}>新建项目</div>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="CB898526-6B98-4287-9FD1-A6CBC655ACD1" className={style.A6CBC655ACD1}><ZeroCodeGridLayout
+            layout={A6CBC655ACD1}>
+            <div key="A6814036-9497-4190-A165-9240F7254AD1" className={style.F7254AD1}><ZeroCodeGridLayout
+              layout={F7254AD1}>
+              <div key="9E929EF8-2D8A-406F-AC9C-84387F706F34" className={style.F706F34}><ZeroCodeGridLayout
+                layout={F706F34}>
+                <div key="945CDBD5-8144-4560-946B-EFF28A979344" className={style.EFF28A979344}></div>
+              </ZeroCodeGridLayout></div>
+              <div key="39B7D273-AEC2-4BC3-8397-4991DF9EF6A5">
+                <div className={style.DF9EF6A5}>Erd数据模型概念草图</div>
+              </div>
+              <div key="52E09C8D-4740-4C22-8CEF-26A17695BD30">
+                <div className={style.A17695BD30}>2021-08-05 09:06:08</div>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="B4C8EBA5-5607-423F-B7FD-91B8E69BF647" className={style.B8E69BF647}><ZeroCodeGridLayout
+            layout={B8E69BF647}>
+            <div key="39E1D573-E7AD-4C0F-B20D-542C61117D62" className={style.C61117D62}><ZeroCodeGridLayout
+              layout={C61117D62}>
+              <div key="9A407BC0-9A57-4A5F-9D38-02965946B4D9">
+                <div className={style.B4D9}>Erd数据模型概念草图</div>
+              </div>
+              <div key="2A859C5B-ADAF-464C-B718-A0732F203EDB">
+                <div className={style.A0732F203EDB}>2021-08-05 09:06:08</div>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="FC579A1A-1993-4B7A-BC1E-9DA1CB00434B" className={style.DA1CB00434B}><ZeroCodeGridLayout
+            layout={DA1CB00434B}>
+            <div key="4FE87FC4-734E-47B5-9417-949D1F781207" className={style.D1F781207}><ZeroCodeGridLayout
+              layout={D1F781207}>
+              <div key="9207B5D5-D650-4409-9255-767975CDD90D">
+                <div className={style.CDD90D}>Erd数据模型概念草图</div>
+              </div>
+              <div key="8401AE3C-7BA4-458D-BB72-02382D88FE9C">
+                <div className={style.D88FE9C}>2021-08-05 09:06:08</div>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="674A3CC1-836B-4BB4-8F5E-2B291DCDC658" className={style.B291DCDC658}><ZeroCodeGridLayout
+            layout={B291DCDC658}>
+            <div key="D698C8A8-A768-4FEC-99B6-6C184E904BF5" className={style.C184E904BF5}><ZeroCodeGridLayout
+              layout={C184E904BF5}>
+              <div key="F456C238-9B7B-427D-846F-7DF2E319B467">
+                <div className={style.DF2E319B467}>Erd数据模型概念草图</div>
+              </div>
+              <div key="9B71E851-F31A-4BFB-8CD6-D87F0E28611C">
+                <div className={style.D87F0E28611C}>2021-08-05 09:06:08</div>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="7207DB2C-C3A7-46CC-91AC-60B49B56FB5F" className={style.B49B56FB5F}><ZeroCodeGridLayout
+            layout={B49B56FB5F}>
+            <div key="1F69E2B7-0C30-470F-8E24-5E5C34A833DE" className={style.E5C34A833DE}><ZeroCodeGridLayout
+              layout={E5C34A833DE}>
+              <div key="744E0283-639B-40CC-9745-CA2A411B5CB3">
+                <div className={style.CA2A411B5CB3}>Erd数据模型概念草图</div>
+              </div>
+              <div key="3927A4D1-EDA4-40DC-A742-A8B57D559CC1">
+                <div className={style.A8B57D559CC1}>2021-08-05 09:06:08</div>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="4335A3E5-37DB-4AF0-8321-9D5EA691B8E3" className={style.D5EA691B8E3}><ZeroCodeGridLayout
+            layout={D5EA691B8E3}>
+            <div key="25DC5439-2B71-47DB-9703-024F3A6F893D" className={style.F3A6F893D}><ZeroCodeGridLayout
+              layout={F3A6F893D}>
+              <div key="DB980A60-DCBE-4E76-A63C-DF98EECBD9BC">
+                <div className={style.DF98EECBD9BC}>Erd数据模型概念草图</div>
+              </div>
+              <div key="CDD588B3-B36E-4A35-8DD7-78EA412C72BD">
+                <div className={style.EA412C72BD}>2021-08-05 09:06:08</div>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="30D8B710-5010-42DD-B86C-9A9C620A72E3" className={style.A9C620A72E3}><ZeroCodeGridLayout
+            layout={A9C620A72E3}>
+            <div key="18582DEF-E5E8-42BE-B5E7-AE339CAE1EB4" className={style.AE339CAE1EB4}><ZeroCodeGridLayout
+              layout={AE339CAE1EB4}>
+              <div key="4C1DC08D-B94F-4CDF-AE08-AAB99A7D54BE">
+                <div className={style.AAB99A7D54BE}>Erd数据模型概念草图</div>
+              </div>
+              <div key="BBCB1A9B-E307-4F22-85AE-7BB7E102EBA6">
+                <div className={style.BB7E102EBA6}>2021-08-05 09:06:08</div>
+              </div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="358EEDC2-070F-4295-8FBC-B62FDB3228AE" className={style.B62FDB3228AE}><ZeroCodeGridLayout
+            layout={B62FDB3228AE}>
+            <div key="6D4850F7-F871-4E61-8FFB-8B09D20E13E3" className={style.B09D20E13E3}></div>
+            <div key="72D02658-E50B-45A2-A238-6940197D04DC" className={style.D04DC}><ZeroCodeGridLayout layout={D04DC}>
+              <div key="16623E90-3B2D-4013-9923-45542442FAA2" className={style.FAA2}><ZeroCodeGridLayout layout={FAA2}>
+                <div key="F48E3118-A667-49BA-837E-C55362B105DA">
+                  <div className={style.C55362B105DA}>1</div>
+                </div>
+              </ZeroCodeGridLayout></div>
+            </ZeroCodeGridLayout></div>
+            <div key="13ECAFFA-6318-4056-9A95-7D1FAD0DB847" className={style.D1FAD0DB847}></div>
+            <div key="61A40040-9799-42D8-B823-2AE41A289265">
+              <div className={style.AE41A289265}>/</div>
+            </div>
+            <div key="DE3175DE-BA51-48A3-BBC0-451B5BDC91D8">
+              <div className={style.B5BDC91D8}>5</div>
+            </div>
+            <div key="841FAFD3-25AA-430E-B6CC-EC5E7DBCDF6F" className={style.EC5E7DBCDF6F}><ZeroCodeGridLayout
+              layout={EC5E7DBCDF6F}>
+              <div key="335EFC7C-4699-4BB5-802F-CFAE62FB6D44" className={style.CFAE62FB6D44}><ZeroCodeGridLayout
+                layout={CFAE62FB6D44}>
+                <div key="B31C3966-C733-4AFC-A55A-E74DE59AC95E" className={style.E74DE59AC95E}></div>
+                <div key="107D0863-9627-4DE8-89A5-0E38EBF3E6C0" className={style.E38EBF3E6C0}></div>
+              </ZeroCodeGridLayout></div>
+            </ZeroCodeGridLayout></div>
+            <div key="227F6D72-B934-4025-A910-281848EB3ADF" className={style.EB3ADF}></div>
+            <div key="A1AD6671-1DBE-4BF8-9644-3CFBE1A9CB30" className={style.CFBE1A9CB30}><ZeroCodeGridLayout
+              layout={CFBE1A9CB30}>
+              <div key="69B69E5F-D2E6-4712-AC7E-B4D96E2A7B2E" className={style.B4D96E2A7B2E}></div>
+            </ZeroCodeGridLayout></div>
+          </ZeroCodeGridLayout></div>
+          <div key="F9AFBD71-04F5-458E-B853-E158176E2B5E" className={style.E158176E2B5E}></div>
+          <div key="F7F7FEB7-138A-414B-84B9-87B9B052826F" className={style.B9B052826F}><ZeroCodeGridLayout
+            layout={B9B052826F}>
+            <div key="6D9A97A5-2F7A-400D-B100-FB695426DAA1" className={style.FB695426DAA1}><ZeroCodeGridLayout
+              layout={FB695426DAA1}>
+              <div key="458536AC-F9D7-4A9B-B376-0B4A8891148F" className={style.B4A8891148F}><ZeroCodeGridLayout
+                layout={B4A8891148F}>
+                <div key="87BE7373-EC1B-4168-846B-3B326C500132" className={style.B326C500132}></div>
+              </ZeroCodeGridLayout></div>
+            </ZeroCodeGridLayout></div>
+            <div key="FB27B04F-F950-40CD-80FC-D93296488FFE">
+              <div className={style.D93296488FFE}>日间</div>
+            </div>
+          </ZeroCodeGridLayout></div>
+        </ZeroCodeGridLayout></div>
+      </>
+    );
+  }
+}
--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -1,16 +1,45 @@
 import React from "react";
 import ZeroCodeGridLayout from "@/components/ZeroCodeGridLayout";
-import {Image} from "antd";
 import style from './index.less';
-import {B57EBF5,EE95921CF001,F5EBA0217,} from './gridLayouts';
+import {B57EBF5, EE95921CF001, F5EBA0217,} from './gridLayouts';
 
 export default class MyFirstGrid extends React.Component {
-    render() {
-        return (
-            <>
-            <div key="320400F6-E186-4B63-9E06-08446B57EBF5" className={style.B57EBF5}><ZeroCodeGridLayout layout={B57EBF5}><div key="412BA199-4634-4993-97CA-EE95921CF001" className={style.EE95921CF001}><ZeroCodeGridLayout layout={EE95921CF001}><div key="BDFACC91-8123-4D4A-89E8-FB94AA6C39A7" ><div className={style.FB94AA6C39A7}>账号
-登录</div></div><div key="898D5E29-B062-4308-BAED-1E85986A488B" className={style.E85986A488B}></div><div key="45D4A20A-D9C0-4F66-A426-896F5EBA0217" className={style.F5EBA0217}><ZeroCodeGridLayout layout={F5EBA0217}><div key="504D50B9-1063-4E43-8214-94ACC58F6C42" ><div className={style.ACC58F6C42}>Genius AFC </div></div><div key="F57CF37E-7029-4E7D-99D7-A7DB1CAE14CC" ><div className={style.A7DB1CAE14CC}>Erd</div></div><div key="1AA65F24-6A32-4E12-9A4E-A416CBEB2E42" className={style.A416CBEB2E42}></div><div key="9F62DDF1-9323-4A9E-8B7C-BA83E55A60D9" className={style.BA83E55A60D9}></div></ZeroCodeGridLayout></div><div key="894B1169-5A2B-4C51-B3F7-4F8FAAFD93E5" ><div className={style.F8FAAFD93E5}>微信登录</div></div><div key="F976E6FC-D920-4C8C-98C7-CCCEB79715D1" className={style.CCCEB79715D1}></div><div key="C20BBA7D-E4F2-480E-AE83-7E1B55906C24" ><div className={style.E1B55906C24}>微信扫码 完成登录</div></div></ZeroCodeGridLayout></div><div key="6D3C4B1E-151C-4973-9768-AFB58EC422E3" ><div className={style.AFB58EC422E3}>Copyright &#169 2012-2021 AFC Genius 沪ICP备 12020087号</div></div></ZeroCodeGridLayout></div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div key="320400F6-E186-4B63-9E06-08446B57EBF5" className={style.B57EBF5}><ZeroCodeGridLayout layout={B57EBF5}>
+          <div key="412BA199-4634-4993-97CA-EE95921CF001" className={style.EE95921CF001}><ZeroCodeGridLayout
+            layout={EE95921CF001}>
+            <div key="BDFACC91-8123-4D4A-89E8-FB94AA6C39A7">
+              <div className={style.FB94AA6C39A7}>账号
+                登录
+              </div>
+            </div>
+            <div key="898D5E29-B062-4308-BAED-1E85986A488B" className={style.E85986A488B}></div>
+            <div key="45D4A20A-D9C0-4F66-A426-896F5EBA0217" className={style.F5EBA0217}><ZeroCodeGridLayout
+              layout={F5EBA0217}>
+              <div key="504D50B9-1063-4E43-8214-94ACC58F6C42">
+                <div className={style.ACC58F6C42}>ERD ONLINE</div>
+              </div>
+              <div key="F57CF37E-7029-4E7D-99D7-A7DB1CAE14CC">
+                <div className={style.A7DB1CAE14CC}></div>
+              </div>
+              <div key="1AA65F24-6A32-4E12-9A4E-A416CBEB2E42" className={style.A416CBEB2E42}></div>
+              <div key="9F62DDF1-9323-4A9E-8B7C-BA83E55A60D9" className={style.BA83E55A60D9}></div>
+            </ZeroCodeGridLayout></div>
+            <div key="894B1169-5A2B-4C51-B3F7-4F8FAAFD93E5">
+              <div className={style.F8FAAFD93E5}>微信登录</div>
+            </div>
+            <div key="F976E6FC-D920-4C8C-98C7-CCCEB79715D1" className={style.CCCEB79715D1}></div>
+            <div key="C20BBA7D-E4F2-480E-AE83-7E1B55906C24">
+              <div className={style.E1B55906C24}>微信扫码 完成登录</div>
+            </div>
+          </ZeroCodeGridLayout></div>
+          <div key="6D3C4B1E-151C-4973-9768-AFB58EC422E3">
+            <div className={style.AFB58EC422E3}>Copyright &ERD ONLINE 2021</div>
+          </div>
+        </ZeroCodeGridLayout></div>
+      </>
+    );
+  }
 }
