e00d90db3f57bab5fbc8cbd85edecf89d2e7adab
liangcan.jxjy.edu@gmail.com <liangcan.jxjy.edu@gmail.com>
2021-10-29 10:52:33 +0800

    设置

---
+++ src/components/dialog/setup/DatabaseSetUp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export type DatabaseSetUpProps = {};
+
+const DatabaseSetUp: React.FC<DatabaseSetUpProps> = (props) => {
+  return (<></>);
+}
+
+export default React.memo(DatabaseSetUp)
---
+++ src/components/dialog/setup/DefaultSetUp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export type DefaultSetUpProps = {};
+
+const DefaultSetUp: React.FC<DefaultSetUpProps> = (props) => {
+  return (<></>);
+}
+
+export default React.memo(DefaultSetUp)
