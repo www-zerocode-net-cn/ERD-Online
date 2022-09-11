ee8cfc4ed7199ec2afe1701894e954bf9d70e64d
www.zerocode.net.cn <www.zerocode.net.cn@gmail.com>
2022-09-11 12:17:24 +0800

    fix(erd): 删除hottable代码

--- src/pages/design/table/component/table/TableInfoEdit.tsx
+++ src/pages/design/table/component/table/TableInfoEdit.tsx
@@ -1,5 +1,4 @@
-import React, {useRef} from 'react';
-import {HotTable} from "@handsontable/react";
+import React from 'react';
 import 'handsontable/dist/handsontable.full.css';
 import "handsontable/languages/zh-CN";
 import useProjectStore from "@/store/project/useProjectStore";
@@ -7,13 +6,6 @@ import {ModuleEntity} from "@/store/tab/useTabStore";
 import shallow from "zustand/shallow";
 // @ts-ignore
 import {CellChange, ChangeSource} from "handsontable";
-import {
-  column1,
-  column2,
-  handsontableAfterChange,
-  handsontableAfterRowMove,
-  handsontableBeforeChange
-} from "@/components/dialog/setup/DefaultField";
 import JExcel from "@/pages/JExcel";
 
 
@@ -24,7 +16,7 @@ export type TableInfoEditProps = {
 
 const TableInfoEdit: React.FC<TableInfoEditProps> = (props) => {
 
-  const {datatype, database, entity, projectDispatch} = useProjectStore(state => ({
+  const {datatype, entity, projectDispatch} = useProjectStore(state => ({
     entity: state.project?.projectJSON?.modules[state.currentModuleIndex || 0]?.entities[state.currentEntityIndex || 0],
     datatype: state.project?.projectJSON?.dataTypeDomains?.datatype,
     database: state.project?.projectJSON?.dataTypeDomains?.database,
@@ -42,64 +34,6 @@ const TableInfoEdit: React.FC<TableInfoEditProps> = (props) => {
   const afterChange = (payload: any) => {
     projectDispatch.updateEntityFields(payload);
   }
-
-  const hotTableComponent = useRef(null);
-
-
-  const hotSettings = {
-    data: JSON.parse(s),
-    columns: [
-      ...column1,
-      {
-        data: 'typeName',
-        type: 'dropdown',
-        source: allDataTypeName,
-        allowInvalid: false,
-        allowEmpty: false
-      },
-      ...column2
-    ],
-    allowInvalid: false,
-    allowRemoveColumn: false,
-    stretchH: "all",
-    height: 700,
-    fixedRowsTop: 0,
-    columnSorting: true,
-    autoWrapRow: true,
-    manualRowResize: true,
-    manualColumnResize: true,
-    rowHeaders: true,
-    colHeaders: [
-      '字段名*',
-      '逻辑名(英文名)*',
-      '类型*',
-      '类型(code)',
-      '数据源类型',
-      '说明',
-      '主键',
-      '非空',
-      '自增',
-      '默认值',
-      '关系图',
-      'UI建议',
-    ],
-    manualRowMove: true,
-    manualColumnMove: true,
-    filters: true,
-    dropdownMenu: true,
-    mergeCells: false,
-    copyPaste: true,
-    language: "zh-CN",
-    licenseKey: 'non-commercial-and-evaluation',
-    className: "htCenter htMiddle",
-    currentRowClassName: 'currentRow',
-    currentColClassName: 'currentCol',
-    customBorders: false,
-    contextMenu: true,
-    allowInsertColumn: false,
-    minRows: 1
-  };
-
 
   const data = JSON.parse(s);
   const columns = [{
@@ -180,7 +114,7 @@ const TableInfoEdit: React.FC<TableInfoEditProps> = (props) => {
         afterRowMove={handsontableAfterRowMove(hotTableComponent, hotSettings, afterChange)}
       >
       </HotTable>*/}
-      <JExcel data={data} columns={columns} saveData={afterChange} notEmptyColumn={['chnname','name','typeName']}/>
+      <JExcel data={data} columns={columns} saveData={afterChange} notEmptyColumn={['chnname', 'name', 'typeName']}/>
     </>
   );
 }
