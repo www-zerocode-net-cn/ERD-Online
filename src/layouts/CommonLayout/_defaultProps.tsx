f98358f94f6df1cdb132111d5ba5daa490a7d3c3
www.zerocode.net.cn <www.zerocode.net.cn@gmail.com>
2022-09-11 18:43:22 +0800

    fix(erd): 修改左上角导航

--- src/layouts/CommonLayout/_defaultProps.tsx
+++ src/layouts/CommonLayout/_defaultProps.tsx
@@ -102,54 +102,20 @@ export default {
   },
   appList: [
     {
-      icon: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-      title: 'Ant Design',
-      desc: '杭州市较知名的 UI 设计语言',
-      url: 'https://ant.design',
-    },
-    {
-      icon: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png',
-      title: 'AntV',
-      desc: '蚂蚁集团全新一代数据可视化解决方案',
-      url: 'https://antv.vision/',
+      icon: '/logo.svg',
+      title: 'ERD Online',
+      desc: '国内第一个开源免费在线建模软件',
+      url: 'https://portal.zerocode.net.cn/',
       target: '_blank',
     },
     {
-      icon: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
-      title: 'Pro Components',
-      desc: '专业级 UI 组件库',
-      url: 'https://procomponents.ant.design/',
-    },
-    {
-      icon: 'https://img.alicdn.com/tfs/TB1zomHwxv1gK0jSZFFXXb0sXXa-200-200.png',
-      title: 'umi',
-      desc: '插件化的企业级前端应用框架。',
-      url: 'https://umijs.org/zh-CN/docs',
+      icon: '/zerocode.svg',
+      title: '零代',
+      desc: '国内第一个零代码社区',
+      url: 'https://www.zerocode.net.cn/',
+      target: '_blank',
     },
 
-    {
-      icon: 'https://gw.alipayobjects.com/zos/bmw-prod/8a74c1d3-16f3-4719-be63-15e467a68a24/km0cv8vn_w500_h500.png',
-      title: 'qiankun',
-      desc: '可能是你见过最完善的微前端解决方案🧐',
-      url: 'https://qiankun.umijs.org/',
-    },
-    {
-      icon: 'https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg',
-      title: '语雀',
-      desc: '知识创作与分享工具',
-      url: 'https://www.yuque.com/',
-    },
-    {
-      icon: 'https://gw.alipayobjects.com/zos/rmsportal/LFooOLwmxGLsltmUjTAP.svg',
-      title: 'Kitchen ',
-      desc: 'Sketch 工具集',
-      url: 'https://kitchen.alipay.com/',
-    },
-    {
-      icon: 'https://img.alicdn.com/tfs/TB1zomHwxv1gK0jSZFFXXb0sXXa-200-200.png',
-      title: 'dumi',
-      desc: '为组件开发场景而生的文档工具',
-      url: 'https://d.umijs.org/zh-CN',
-    },
+
   ],
 };
