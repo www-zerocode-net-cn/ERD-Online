ead777a9281ca4b57856081f0b1e255b87ed08fb
www.zerocode.net.cn <www.zerocode.net.cn@gmail.com>
2022-09-24 18:35:44 +0800

    feat(erd): 普通导出功能

---
+++ src/pages/design/export/component/ExportCommon.tsx
@@ -0,0 +1,74 @@
+import {ProList} from '@ant-design/pro-components';
+import {Tag} from 'antd';
+import {FileMarkdownTwoTone, FileTextTwoTone, FileWordTwoTone, Html5TwoTone} from "@ant-design/icons";
+import useProjectStore from "@/store/project/useProjectStore";
+import shallow from "zustand/shallow";
+
+
+export default () => {
+
+  const data = [
+    {
+      key: 'JSON',
+      title: '导出ERD',
+      subTitle: <Tag color="blue">ERD</Tag>,
+      avatar: <FileTextTwoTone/>,
+      content: (
+        <div style={{marginLeft: '45px'}}>导出一个ERD格式的文件，文本内容已加密，可再次导入ERD系统</div>
+      ),
+    }, {
+      key: 'Html',
+      title: '导出HTML',
+      subTitle: <Tag color="blue">HTML</Tag>,
+      avatar: <Html5TwoTone/>,
+      content: (
+        <div style={{marginLeft: '45px'}}>导出一个可以在任意浏览器中打开的HTML文件</div>
+      ),
+    }, {
+      key: 'Word',
+      title: '导出Word',
+      subTitle: <Tag color="blue">Word</Tag>,
+      avatar: <FileWordTwoTone/>,
+      content: (
+        <div style={{marginLeft: '45px'}}>导出一个漂亮的word文件，包含表元数据和关系图</div>
+      ),
+    }, {
+      key: 'Markdown',
+      title: '导出Markdown',
+      subTitle: <Tag color="blue">Markdown</Tag>,
+      avatar: <FileMarkdownTwoTone/>,
+      content: (
+        <div style={{marginLeft: '45px'}}>导出一个Markdown文件，可以在任意Markdown编辑器中预览</div>
+      ),
+    },
+  ];
+
+  const {projectDispatch} = useProjectStore(state => ({
+    projectDispatch: state.dispatch,
+  }), shallow);
+
+  return (
+
+    <ProList<any>
+      grid={{gutter: 16, column: 2}}
+      onItem={(record: any) => {
+        return {
+          onClick: () => {
+            console.log(record);
+            projectDispatch.exportFile(record.key)
+          },
+        };
+      }}
+      metas={{
+        title: {},
+        subTitle: {},
+        type: {},
+        avatar: {},
+        content: {},
+      }}
+      headerTitle="导出文件"
+      tooltip="单击下方区块即可导出"
+      dataSource={data}
+    />
+  );
+};
