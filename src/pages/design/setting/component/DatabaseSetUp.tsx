11b48e3716bed5cceb172fabdd5294a1dc5d323f
www.zerocode.net.cn <www.zerocode.net.cn@gmail.com>
2022-09-24 19:23:23 +0800

    fix(erd): 数据源设置

--- src/pages/design/setting/component/DatabaseSetUp.tsx
+++ src/pages/design/setting/component/DatabaseSetUp.tsx
@@ -1,21 +1,20 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {
-  ModalForm,
+  ProCard,
+  ProForm,
   ProFormGroup,
   ProFormInstance,
   ProFormList,
   ProFormRadio,
   ProFormSelect,
   ProFormText
-} from "@ant-design/pro-form";
-import {Alignment, Button} from "@blueprintjs/core";
+} from "@ant-design/pro-components";
 import _ from "lodash";
-import {Button as MuiButton, Grid} from "@mui/material";
 import useProjectStore from "@/store/project/useProjectStore";
 import shallow from "zustand/shallow";
 import {uuid} from '@/utils/uuid';
-import {DeleteOutlined, PlusOutlined} from '@ant-design/icons';
-import {Button as AntButton, message, Popconfirm} from 'antd';
+import {DeleteOutlined} from '@ant-design/icons';
+import {Button, message, Popconfirm} from 'antd';
 import * as Save from '@/utils/save';
 
 export type DatabaseSetUpProps = {};
@@ -126,19 +125,8 @@ const DatabaseSetUp: React.FC<DatabaseSetUpProps> = (props) => {
 
 
   return (<>
-      <ModalForm
+      <ProForm
         formRef={formRef}
-        title={<span>数据源连接配置</span>}
-        trigger={
-          <Button
-            key="db"
-            icon="database"
-            text="数据源设置"
-            minimal={true}
-            small={true}
-            fill={true}
-            alignText={Alignment.LEFT}></Button>
-        }
         initialValues={{
           ...defaultDbs?.properties,
           dbs: tempDBs
@@ -146,220 +134,213 @@ const DatabaseSetUp: React.FC<DatabaseSetUpProps> = (props) => {
         // 完全自定义整个区域
         submitter={{
           // 完全自定义整个区域
-          render: (props, doms) => {
+          render: (props) => {
             console.log(props);
-            // @ts-ignore
             return _.concat([], [
-              <MuiButton disabled={!defaultData} variant="outlined" color="warning" key="rest"
-                         onClick={() => connectJDBC()}>{state.loading ? "正在连接" : "测试"}</MuiButton>,
-              <MuiButton variant="contained" key="submit" onClick={() => {
-                message.success("保存成功！");
-              }}> 确定 </MuiButton>,
+              <Button type="dashed"
+                      onClick={() => {
+                        projectDispatch.addDbs({
+                          name: '',
+                          select: defaultDatabase,
+                          key: uuid(),
+                          defaultDB: tempDBs.findIndex((db: any) => db.defaultDB) === -1,
+                          properties: {
+                            driver_class_name: defaultDBData.driver_class_name,
+                            url: defaultDBData.url,
+                            password: '',
+                            username: ''
+                          }
+                        });
+                      }}>新增</Button>,
+              <Button disabled={!defaultData} key="rest"
+                      onClick={() => connectJDBC()}>{state.loading ? "正在连接" : "测试"}</Button>,
             ]);
           },
         }}
       >
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <ProFormList
-              name="dbs"
-              creatorButtonProps={false}
-              label={
-                <span>{defaultDB ? ` 当前使用的数据源为「${defaultDB.name}」` : tempDBs.length > 0 ? ' 当前未选择默认数据源' : '当前未创建数据源'}</span>}
-              itemRender={
-                ({listDom, action}, {record}) => {
-                  console.log(147, 'record', record);
-                  return (
-                    <ProFormGroup size={8}>
-                      <ProFormRadio
-                        name="defaultDB"
-                        fieldProps={{
-                          onChange: () => {
-                            projectDispatch.setDefaultDb(record.key)
-                          }
-                        }}/>
-                      <ProFormSelect
-                        options={databaseSelect || []}
-                        name="select"
-                        fieldProps={{
-                          disabled: !record.defaultDB,
-                          onChange: (value: any, option: any) => {
-                            console.log(166, value, option);
-                            projectDispatch.updateDbs('select', value);
-                            projectDispatch.updateDbs('properties', {
-                              driver_class_name: url[value.toLowerCase()].driver_class_name,
-                              url: url[value.toLowerCase()].url,
-                              username: '',
-                              password: ''
-                            });
-                          }
-                        }}
-                      />
-                      <ProFormText
-                        name="name"
-                        fieldProps={{
-                          size: "small",
-                          disabled: !record.defaultDB,
-                          onBlur: (e) => {
-                            console.log(182, e.target.value);
-                            projectDispatch.updateDbs('name', e.target.value);
-                          }
-                        }}
-                        rules={[
-                          {
-                            required: true,
-                          },
-                        ]}
-                      />
-                      <Popconfirm
-                        title={record.defaultDB ? "是否要删除默认数据源？删除之后，系统将不存在默认数据源！" : "是否删除该数据源？"}
-                        onConfirm={() => projectDispatch.removeDbs(record.key)}
-                        okText="是"
-                        cancelText="否"
-                      >
-                        <a><DeleteOutlined title={"删除"}/></a>
-                      </Popconfirm>
-                    </ProFormGroup>
-                  );
+        <ProCard direction="column" ghost gutter={[0, 16]}>
+          <ProCard gutter={16} ghost>
+            <ProCard colSpan={12} ghost>
+              <ProFormList
+                name="dbs"
+                creatorButtonProps={false}
+                label={
+                  <span>{defaultDB ? ` 当前使用的数据源为「${defaultDB.name}」` : tempDBs.length > 0 ? ' 当前未选择默认数据源' : '当前未创建数据源'}</span>}
+                itemRender={
+                  ({listDom, action}, {record}) => {
+                    console.log(147, 'record', record);
+                    return (
+                      <ProFormGroup size={8}>
+                        <ProFormRadio
+                          name="defaultDB"
+                          fieldProps={{
+                            onChange: () => {
+                              projectDispatch.setDefaultDb(record.key)
+                            }
+                          }}/>
+                        <ProFormSelect
+                          options={databaseSelect || []}
+                          name="select"
+                          fieldProps={{
+                            disabled: !record.defaultDB,
+                            onChange: (value: any, option: any) => {
+                              console.log(166, value, option);
+                              projectDispatch.updateDbs('select', value);
+                              projectDispatch.updateDbs('properties', {
+                                driver_class_name: url[value.toLowerCase()].driver_class_name,
+                                url: url[value.toLowerCase()].url,
+                                username: '',
+                                password: ''
+                              });
+                            }
+                          }}
+                        />
+                        <ProFormText
+                          name="name"
+                          fieldProps={{
+                            size: "small",
+                            disabled: !record.defaultDB,
+                            onBlur: (e) => {
+                              console.log(182, e.target.value);
+                              projectDispatch.updateDbs('name', e.target.value);
+                            }
+                          }}
+                          rules={[
+                            {
+                              required: true,
+                            },
+                          ]}
+                        />
+                        <Popconfirm
+                          title={record.defaultDB ? "是否要删除默认数据源？删除之后，系统将不存在默认数据源！" : "是否删除该数据源？"}
+                          onConfirm={() => projectDispatch.removeDbs(record.key)}
+                          okText="是"
+                          cancelText="否"
+                        >
+                          <a><DeleteOutlined title={"删除"}/></a>
+                        </Popconfirm>
+                      </ProFormGroup>
+                    );
+                  }
                 }
-              }
-              copyIconProps={false}
-            >
-              <></>
-            </ProFormList>
-            <AntButton type="dashed" block icon={<PlusOutlined/>}
-                       onClick={() => {
-                         projectDispatch.addDbs({
-                           name: '',
-                           select: defaultDatabase,
-                           key: uuid(),
-                           defaultDB: tempDBs.findIndex((db: any) => db.defaultDB) === -1,
-                           properties: {
-                             driver_class_name: defaultDBData.driver_class_name,
-                             url: defaultDBData.url,
-                             password: '',
-                             username: ''
-                           }
-                         });
-                       }}>新增数据源</AntButton>
+                copyIconProps={false}
+              />
 
-          </Grid>
-          <Grid item xs={6}>
-            <ProFormText
-              width="md"
-              name="driver_class_name"
-              label="driver_class_name"
-              placeholder="driver_class_name"
-              fieldProps={{
-                onBlur: (e) => {
-                  console.log(225, e.target.value);
-                  projectDispatch.updateDbs('properties', {
-                    ...defaultDbs.properties,
-                    driver_class_name: e.target.value
-                  });
-                }
-              }}
-              formItemProps={{
-                rules: [
-                  {
-                    required: true,
-                    message: '不能为空',
-                  },
-                  {
-                    max: 300,
-                    message: '不能大于 300 个字符',
-                  },
-                ],
+            </ProCard>
+            <ProCard colSpan={12} ghost>
+              <ProFormText
+                width="lg"
+                name="driver_class_name"
+                label="driver_class_name"
+                placeholder="driver_class_name"
+                fieldProps={{
+                  onBlur: (e) => {
+                    console.log(225, e.target.value);
+                    projectDispatch.updateDbs('properties', {
+                      ...defaultDbs.properties,
+                      driver_class_name: e.target.value
+                    });
+                  }
+                }}
+                formItemProps={{
+                  rules: [
+                    {
+                      required: true,
+                      message: '不能为空',
+                    },
+                    {
+                      max: 300,
+                      message: '不能大于 300 个字符',
+                    },
+                  ],
 
-              }}
-            />
-            <ProFormText
-              width="md"
-              name="url"
-              label="url"
-              placeholder="请输入url"
-              fieldProps={{
-                onBlur: (e) => {
-                  console.log(254, e.target.value);
-                  projectDispatch.updateDbs('properties', {
-                    ...defaultDbs.properties,
-                    url: e.target.value
-                  });
-                }
-              }}
-              formItemProps={{
-                rules: [
-                  {
-                    required: true,
-                    message: '不能为空',
-                  },
-                  {
-                    max: 300,
-                    message: '不能大于 300 个字符',
-                  },
-                ],
-              }}
-            />
-            <ProFormText
-              width="md"
-              name="username"
-              label="username"
-              placeholder="请输入username"
-              fieldProps={{
-                onBlur: (e) => {
-                  console.log(281, e.target.value);
-                  projectDispatch.updateDbs('properties', {
-                    ...defaultDbs.properties,
-                    username: e.target.value
-                  });
-                }
-              }}
-              formItemProps={{
-                rules: [
-                  {
-                    required: true,
-                    message: '不能为空',
-                  },
-                  {
-                    max: 100,
-                    message: '不能大于 100 个字符',
-                  },
-                ],
-              }}
-            />
-            <ProFormText.Password
-              width="md"
-              name="password"
-              label="password"
-              placeholder="请输入password"
-              fieldProps={{
-                onBlur: (e) => {
-                  console.log(308, e.target.value);
-                  projectDispatch.updateDbs('properties', {
-                    ...defaultDbs.properties,
-                    password: e.target.value
-                  });
-                }
-              }}
-              formItemProps={{
-                rules: [
-                  {
-                    required: true,
-                    message: '不能为空',
-                  },
-                  {
-                    max: 100,
-                    message: '不能大于 100 个字符',
-                  },
-                ],
-              }}
-            />
-          </Grid>
-        </Grid>
-
-      </ModalForm>
-
-
+                }}
+              />
+              <ProFormText
+                width="lg"
+                name="url"
+                label="url"
+                placeholder="请输入url"
+                fieldProps={{
+                  onBlur: (e) => {
+                    console.log(254, e.target.value);
+                    projectDispatch.updateDbs('properties', {
+                      ...defaultDbs.properties,
+                      url: e.target.value
+                    });
+                  }
+                }}
+                formItemProps={{
+                  rules: [
+                    {
+                      required: true,
+                      message: '不能为空',
+                    },
+                    {
+                      max: 300,
+                      message: '不能大于 300 个字符',
+                    },
+                  ],
+                }}
+              />
+              <ProFormText
+                width="lg"
+                name="username"
+                label="username"
+                placeholder="请输入username"
+                fieldProps={{
+                  onBlur: (e) => {
+                    console.log(281, e.target.value);
+                    projectDispatch.updateDbs('properties', {
+                      ...defaultDbs.properties,
+                      username: e.target.value
+                    });
+                  }
+                }}
+                formItemProps={{
+                  rules: [
+                    {
+                      required: true,
+                      message: '不能为空',
+                    },
+                    {
+                      max: 100,
+                      message: '不能大于 100 个字符',
+                    },
+                  ],
+                }}
+              />
+              <ProFormText.Password
+                width="lg"
+                name="password"
+                label="password"
+                placeholder="请输入password"
+                fieldProps={{
+                  onBlur: (e) => {
+                    console.log(308, e.target.value);
+                    projectDispatch.updateDbs('properties', {
+                      ...defaultDbs.properties,
+                      password: e.target.value
+                    });
+                  }
+                }}
+                formItemProps={{
+                  rules: [
+                    {
+                      required: true,
+                      message: '不能为空',
+                    },
+                    {
+                      max: 100,
+                      message: '不能大于 100 个字符',
+                    },
+                  ],
+                }}
+              />
+            </ProCard>
+          </ProCard>
+        </ProCard>
+      </ProForm>
     </>
   );
 };
