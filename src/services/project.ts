9a5777aaede5e8bf98e8eabd76887fce31372bef
www.zerocode.net.cn <www.zerocode.net.cn@gmail.com>
2022-10-15 18:15:14 +0800

    feat(erd): 增加项目修改、删除、打开模型service

---
+++ src/services/project.ts
@@ -0,0 +1,44 @@
+import * as cache from '@/utils/cache';
+
+import request from "@/utils/request";
+
+
+// 新增项目
+export const addProject = (data: any) => {
+  return request.post('/ncnb/project/add', {data: data});
+};
+
+// 修改项目
+export const updateProject = (data: any) => {
+  return request.post('/ncnb/project/update', {data: data});
+};
+
+// 删除项目
+export const deleteProject = (data: any) => {
+  return request.post('/ncnb/project/delete', {data: data});
+};
+
+// 查询项目
+export const pageProject = (params: any) => {
+  return request.get('/ncnb/project/page', {
+    params: {
+      page: params.page,
+      limit: params.limit,
+      projectName: params.projectName,
+      order: params.order
+    }
+  });
+};
+
+// 保存项目
+export const saveProject = (data: any) => {
+  const id = cache.getItem('projectId');
+  return request.post('/ncnb/project/save', {
+    data: {
+      ...data,
+      id
+    }
+  });
+};
+
+
